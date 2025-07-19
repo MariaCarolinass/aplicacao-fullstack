c53add8ffe855d5e8792ffc62061a6928cebb0fa
mariacarolinass <m.carolina.soares1@gmail.com>
2025-07-19 17:02:51 -0300

    add dto para relatorio

---
+++ vendasonline/src/main/java/com/shop/vendasonline/dto/RelatorioDTO.java
@@ -0,0 +1,12 @@
+package com.shop.vendasonline.dto;
+
+import lombok.Data;
+
+@Data
+public class RelatorioDTO {
+
+    private long totalPedidos;
+    private double valorTotalFaturado;
+    private long quantidadeProdutosVendidos;
+    
+}

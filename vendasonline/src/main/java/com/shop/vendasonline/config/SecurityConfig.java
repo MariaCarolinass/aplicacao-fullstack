7492ec1afc9246b16828548346f272d2d14d471d
Carolina Soares <m.carolina.soares1@gmail.com>
2025-07-22 04:56:55 -0300

    alterei url api SecurityConfig.java

--- vendasonline/src/main/java/com/shop/vendasonline/config/SecurityConfig.java
+++ vendasonline/src/main/java/com/shop/vendasonline/config/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOriginPatterns(List.of(
             "http://localhost:*",
-            "https://aplicacao-fullstack.fly.dev"
+            "https://aplicacao-fullstack-production.up.railway.app/"
         ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
